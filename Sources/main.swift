59bc7136b2e2b84ff54dfcaefbddc2da13a2d465
YUJINHAI2015 <1394162507@qq.com>
2017-03-15 15:54:37 +0800

    CommandKit and Rainbow

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,60 @@
 // 可执行文件的入口
+import Foundation
+import CommandLineKit   // 接受字符输入
+import Rainbow          // 显示高亮
 
-print("Hello, world!")
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(
+    shortFlag: "p", longFlag: "project",required: true,
+    helpMessage: "Path to the output file.")
+let resourceExtensionOption = MultiStringOption(
+    shortFlag: "r", longFlag: "resource-extensions",
+    helpMessage: "Use data compression.")
+let fileExtensionsOption = MultiStringOption(
+    shortFlag: "f", longFlag: "file-extensions", 
+    helpMessage:"file extensions to search with.")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+cli.addOptions(projectOption, resourceExtensionOption, fileExtensionsOption,help)
+
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EX_USAGE)
+}
+
+let project = projectOption.value ?? "."
+let resourceExtensions = resourceExtensionOption.value ?? ["png","jpg","imageset"]
+let fileExtensions = fileExtensionsOption.value ?? ["swift","m","mm","xib","storyboard"]
+
+
+
+
+
+
+
